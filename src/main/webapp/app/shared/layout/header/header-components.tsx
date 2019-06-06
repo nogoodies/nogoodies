da0f38483a846405b1254a04eb80397f8337483b
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2019-06-06 17:52:22 +0200

    Make logo bigga

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -7,7 +7,7 @@ import { faDumpsterFire } from '@fortawesome/free-solid-svg-icons';
 export function Brand() {
   return (
     <NavbarBrand tag={Link} to="/" className="brand-logo">
-      <FontAwesomeIcon className="brand-icon" icon={faDumpsterFire} />
+      <FontAwesomeIcon className="brand-icon" icon={faDumpsterFire} size="lg" />
       <span className="brand-title">No Goodies</span>
     </NavbarBrand>
   );
