b4a83319600b4ebf6e8b2f33f02830f5c49057a2
Pierre Guillot <pierre.guillot@sonarsource.com>
2019-06-06 14:06:15 +0200

    init commit

---
+++ src/main/java/com/sonarsource/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.sonarsource.config.audit;
---
+++ src/main/java/com/sonarsource/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.sonarsource.repository;
+
+import com.sonarsource.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
